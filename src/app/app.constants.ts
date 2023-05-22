7648a0ebe6e16be31d8230c05a847f4bf284097b
RashaEldebeeky <Rashaeldebeeky@gmail.com>
2023-05-22 15:57:08 +0100

    Initial Commit

---
+++ src/app/app.constants.ts
@@ -0,0 +1,16 @@
+import { faPause, faPlay, faRepeat } from '@fortawesome/free-solid-svg-icons';
+
+export const buttonModes = {
+  play: {
+    title: 'Play',
+    icon: faPlay,
+  },
+  pause: {
+    title: 'Pause',
+    icon: faPause,
+  },
+  repeat: {
+    title: 'Repeat',
+    icon: faRepeat,
+  },
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HighchartsChartModule } from 'highcharts-angular';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MatSliderModule } from '@angular/material/slider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HighchartsChartModule,
+    FontAwesomeModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
